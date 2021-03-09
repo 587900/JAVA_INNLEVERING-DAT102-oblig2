14fddcac803f22d09dcb04c47e0c86ea9690f122
587900 <587900@stud.hvl.no>
2021-03-09 15:00:37 +0100

    test fixes

--- JAVA_INNLEVERING-DAT102-oblig2/test/no/hvl/dat102/mengde/MengdeTest.java
+++ JAVA_INNLEVERING-DAT102-oblig2/test/no/hvl/dat102/mengde/MengdeTest.java
@@ -3,7 +3,7 @@ package no.hvl.dat102.mengde;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
 import no.hvl.dat102.mengde.adt.MengdeADT;
@@ -118,11 +118,14 @@ public abstract class MengdeTest {
 	public final void differensUlikTest1() {
 		for (int i = 9; i <= 15; i++) m2.leggTil(i);
 
-		MengdeADT<Integer> fasit = getNewMengde();
-		for (int i = 3; i <= 8; i++) fasit.leggTil(i);
+		MengdeADT<Integer> fasit1 = getNewMengde();
+		for (int i = 3; i <= 8; i++) fasit1.leggTil(i);
 		
-		assertTrue(fasit.equals(m1.differens(m2)));
-		assertTrue(fasit.equals(m2.differens(m1)));
+		MengdeADT<Integer> fasit2 = getNewMengde();
+		for (int i = 9; i <= 15; i++) fasit2.leggTil(i);
+		
+		assertTrue(fasit1.equals(m1.differens(m2)));
+		assertTrue(fasit2.equals(m2.differens(m1)));
 		
 	}
 	
@@ -130,11 +133,14 @@ public abstract class MengdeTest {
 	public final void differensUlikTest2() {
 		for (int i = 1; i <= 5; i++) m2.leggTil(i);
 
-		MengdeADT<Integer> fasit = getNewMengde();
-		for (int i = 6; i <= 8; i++) fasit.leggTil(i);
+		MengdeADT<Integer> fasit1 = getNewMengde();
+		for (int i = 6; i <= 8; i++) fasit1.leggTil(i);
 		
-		assertTrue(fasit.equals(m1.differens(m2)));
-		assertTrue(fasit.equals(m2.differens(m1)));
+		MengdeADT<Integer> fasit2 = getNewMengde();
+		for (int i = 1; i <= 2; i++) fasit2.leggTil(i);
+		
+		assertTrue(fasit1.equals(m1.differens(m2)));
+		assertTrue(fasit2.equals(m2.differens(m1)));
 		
 	}
 
